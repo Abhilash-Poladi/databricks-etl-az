9eef9177450274920fd2b8288f8a6e99b74c2abb
Abhilash-Poladi <abhilashraopoladi7@gmail.com>
2026-08-17 14:53:06 +0000

    updating dlt pipeline

--- az_bundle/dlt/first_dlt_pipeline/transformations/my_transformation.py
+++ az_bundle/dlt/first_dlt_pipeline/transformations/my_transformation.py
@@ -5,7 +5,8 @@ from pyspark.sql.functions import *
 def orders_bronze():
     return spark.read.table("ap_databricks_ws.default.orders_raw")
 
-@dlt.expect("valid_order_id", "amount > 200")
+@dlt.expect("valid_order_id", "id is not null")
+@dlt.expect("valid_amount", "amount > 0")
 @dlt.table(name="orders_silver")
 def orders_silver():
     return spark.read.table("ap_databricks_ws.default.orders_bronze")
